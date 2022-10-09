d0bdb9c8470227a93292cce5f3329c4cbae329aa
simeon2941 <simeonlico@gmail.com>
2022-10-08 21:40:04 -0500

    TourOfHeroesApp

--- TourOfHeroes/src/app/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
 
 @Component({
@@ -9,22 +9,31 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+  constructor(private heroService: HeroService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
-  
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 
 }
